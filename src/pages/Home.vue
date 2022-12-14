430ef933e941a0a6711e828de254f44fa6fe0382
thang.cao <thang.cao@9thwonder.com>
2022-12-14 11:28:48 +0700

    fix generate device uuid

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,20 @@
 import { onMounted, ref } from 'vue';
 import MemberServices from '../firebase/member/member'
 import DeviceServices from '../firebase/device/device'
-import { DeviceUUID } from 'device-uuid'
 
-const uuid = new DeviceUUID().get();
+const generateDeviceUUID = () => {
+  var navigator_info = window.navigator;
+  var screen_info = window.screen;
+  var uid = navigator_info.mimeTypes.length;
+  uid += navigator_info.userAgent.replace(/\D+/g, '');
+  uid += navigator_info.plugins.length;
+  uid += screen_info.height || '';
+  uid += screen_info.width || '';
+  uid += screen_info.pixelDepth || '';
+  return uid
+}
+
+const uuid = generateDeviceUUID()
 const inputName = ref('')
 const name = ref('')
 const isMounted = ref(false)
